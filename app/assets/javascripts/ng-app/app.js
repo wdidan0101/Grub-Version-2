186d3d85f3c2a7f747a80411fe4e20651a5f091a
wdidan0101 <wdidan0101@gmail.com>
2014-12-30 02:57:57 -0800

    nav-tabs modified

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -5,12 +5,20 @@ var Grub = angular.module('Grub', ['ngAnimate','ui.router','templates']);
         /* Route and States */
         $stateProvider
         .state('home', {
-            url: '/',
+            url: '/home',
             templateUrl: 'home.html',
             controller: 'LoginCtrl'
+        })
+        .state('home.sign-in', { // nested list with custom controller
+            url: '/sign-in',
+            templateUrl: 'sign-in.html'
+        })
+        .state('home.sign-up', { // nested list with just some random string data
+            url: '/sign-up',
+            templateUrl: 'sign-up.html'
         });
 
-        $urlRouterProvider.otherwise('/'); // default fallback route
+        $urlRouterProvider.otherwise('/home'); // default fallback route
         $locationProvider.html5Mode({ // enable HTML5 mode for SEO
           enabled: true,
           requireBase: false
--- app/assets/javascripts/ng-app/controllers/LoginCtrl.js
+++ app/assets/javascripts/ng-app/controllers/LoginCtrl.js
@@ -1,13 +1,20 @@
-angular.module('Grub')
-    .controller('LoginCtrl', function ($scope) {
+Grub
+  .controller('LoginCtrl', function ($scope) {
         
         var self = this;
-      	self.change = function() {
-      		self.username = 'changed';
-      		self.password = 'password';
-      	};
       	self.submit = function() {
       		console.log('User clicked submit with ',
       			self.user);
       	};
+
+      	self.tab = 1;
+       	self.setTab = function(selectedTab){
+            self.tab = selectedTab;
+        };
+       	self.isSet = function(checkTab){
+          return this.tab === checkTab;
+        };
     });
+
+
+        
